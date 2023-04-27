cf7b861786eb8213273b00b459d90b7cd7dd6e48
Joshua Vargas Chia <jvargaschia@gmail.com>
2023-04-26 20:40:51 -0400

    primer commit

---
+++ funciones.js
@@ -0,0 +1,15 @@
+function validarCampo() {
+    let campos = [["name", "nameLbl"], ["correo", "correoLbl"], ["mensaje", "mensajeLbl"]];
+    var vacios = []
+    for (var i=0; i < campos.length; i++){
+        let campo = document.forms["contacto"][campos[i][0]].value;
+        let etiqueta = document.getElementById(campos[i][1]);
+        if (campo == "") {
+            vacios.push(etiqueta.innerHTML)
+        }
+    };
+    if (vacios.length > 0){
+        alert("Debe llenar los siguientes campos:\n " + vacios.join(", "));
+    }
+    return false;
+}
